8f0c76bc72e5ddd41a540994bcc7308655676b31
SZanlongo <SZanlongo@users.noreply.github.com>
2017-10-05 16:48:16 -0400

    Updated examples to work with r-trees

--- examples/example_3d_rrt.py
+++ examples/example_3d_rrt.py
@@ -6,9 +6,10 @@ from src.rrt.rrt import rrt_tree_path
 from src.utilities.plotting import Plot
 
 q = 10  # length of tree edges
+r = 0.1  # length of smallest edge to check for intersection with obstacles
 # obstacles
-O = [(20, 20, 20, 40, 40, 40), (20, 20, 60, 40, 40, 80), (20, 60, 20, 40, 80, 40), (60, 60, 20, 80, 80, 40),
-     (60, 20, 20, 80, 40, 40), (60, 20, 60, 80, 40, 80), (20, 60, 60, 40, 80, 80), (60, 60, 60, 80, 80, 80)]
+Obstacles = [(20, 20, 20, 40, 40, 40), (20, 20, 60, 40, 40, 80), (20, 60, 20, 40, 80, 40), (60, 60, 20, 80, 80, 40),
+             (60, 20, 20, 80, 40, 40), (60, 20, 60, 80, 40, 80), (20, 60, 60, 40, 80, 80), (60, 60, 60, 80, 80, 80)]
 x_init = (0, 0, 0)  # starting location
 x_goal = (100, 100, 100)  # goal location
 n = 100  # number of samples to take each iteration
@@ -16,16 +17,16 @@ max_samples = 5000  # max number of samples to take before timing out
 X_dimensions = [(0, 100), (0, 100), (0, 100)]  # dimensions of Configuration Space
 
 # create Configuration Space
-X = ConfigurationSpace(X_dimensions, O)
+X = ConfigurationSpace(X_dimensions, Obstacles)
 
 # create rrt
-E, path = rrt_tree_path(X, x_init, n, max_samples, q, x_goal)
+E, path = rrt_tree_path(X, x_init, n, max_samples, q, r, x_goal)
 
 # plot
 plot = Plot("example_3d_rrt")
 plot.plot_tree(X, E)
 plot.plot_path(X, path)
-plot.plot_obstacles(X, O)
+plot.plot_obstacles(X, Obstacles)
 plot.plot_start(X, x_init)
 plot.plot_goal(X, x_goal)
 plot.draw()
