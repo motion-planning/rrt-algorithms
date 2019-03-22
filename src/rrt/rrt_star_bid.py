20823910780de7d7e6c0d505cb7460c78f9c03f4
tahsinkose <tahsinkose@yandex.com>
2019-03-22 23:07:54 +0300

    No need rrt_star_rewire. Remove deprecated rrt_star_rewire

--- src/rrt/rrt_star_bid.py
+++ src/rrt/rrt_star_bid.py
@@ -4,10 +4,10 @@
 import random
 
 from src.rrt.heuristics import path_cost
-from src.rrt.rrt_star_rewire import RRTStarRewire
+from src.rrt.rrt_star import RRTStar
 
 
-class RRTStarBidirectional(RRTStarRewire):
+class RRTStarBidirectional(RRTStar):
     def __init__(self, X, Q, x_init, x_goal, max_samples, r, prc=0.01, rewire_count=None):
         """
         Bidirectional RRT* Search
